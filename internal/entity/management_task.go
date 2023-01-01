2838131023db6f6516fc7bbdf200fa59ef0b95c1
Luciano Ferrari <lucianodarioferrari@gmail.com>
2023-01-01 17:46:34 -0300

    binding fix

--- internal/entity/management_task.go
+++ internal/entity/management_task.go
@@ -1,30 +1,30 @@
 package entity
 
 type Issue struct {
-	Title       string `json:"title,required"`
-	Description string `json:"description,required"`
+	Title       string `json:"title" binding:"required"`
+	Description string `json:"description" binding:"required"`
 }
 
 type Bug struct {
-	Description string `json:"description,required"`
+	Description string `json:"description" binding:"required"`
 }
 
 type Task struct {
-	Title    string `json:"title,required"`
+	Title    string `json:"title"binding:"required"`
 	Category string `json:"category"`
 }
 
 type InternalCard struct {
-	Name     string   `json:"name,required"`
+	Name     string   `json:"name" binding:"required"`
 	Desc     string   `json:"desc"`
-	IdLabels []string `json:"idLabels,required"`
+	IdLabels []string `json:"idLabels" binding:"required"`
 }
 
 type Card struct {
-	Name      string   `json:"name,required"`
+	Name      string   `json:"name" binding:"required"`
 	Desc      string   `json:"desc"`
-	IdList    string   `json:"idList,required"`
-	IdLabels  []string `json:"idLabels,required"`
+	IdList    string   `json:"idList"`
+	IdLabels  []string `json:"idLabels"`
 	ShortLink string   `json:"shortLink"`
 	ShortURL  string   `json:"shortUrl"`
 	URL       string   `json:"url"`
