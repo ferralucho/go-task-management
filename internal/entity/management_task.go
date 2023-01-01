1fef5d593025b8e8e3b25ab83aa352e3dc517e93
Luciano Ferrari <lucianodarioferrari@gmail.com>
2023-01-01 10:09:29 -0300

    management task

--- internal/entity/management_task.go
+++ internal/entity/management_task.go
@@ -1,16 +1,32 @@
 package entity
 
 type Issue struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	Title       string `json:"title,required"`
+	Description string `json:"description,required"`
 }
 
 type Bug struct {
 	Title       string `json:"title"`
-	Description string `json:"description"`
+	Description string `json:"description,required"`
 }
 
 type Task struct {
-	Title    string `json:"title"`
+	Title    string `json:"title,required"`
 	Category string `json:"category"`
 }
+
+type Category string
+
+const (
+	Maintenance Category = "maintenance"
+	Research    Category = "research"
+	Test        Category = "test"
+)
+
+type TaskType string
+
+const (
+	TypeIssue TaskType = "issue"
+	TypeBug   TaskType = "bug"
+	TypeTask  TaskType = "task"
+)
